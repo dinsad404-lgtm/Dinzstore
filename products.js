99621eed452cff04493580f8452f9fda613419c3
dinsad404-lgtm <dinsad404@gmail.com>
2025-09-09 08:33:50 +0700

    Update products.js
    Signed-off-by: dinsad404-lgtm <dinsad404@gmail.com>

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 // Edit daftar produk di bawah ini sesuai list harga kamu.
 // Contoh format item: { code: 'A1', name: 'Diamond ML 86', price: 26950, desc: 'Instant proses' }
 const PRODUCTS = [
-  { code: 'P1', name: 'NETFLIX', price: 14.500, desc: 'NETFLIX Sharing 1p2u' },
-  { code: 'p2', name: 'CAPCUT', price: 10.000, desc: 'CAPCUT Private 1 bulan gar 7h' },
-  { code: 'p3', name: 'CANVA', price: 1.500, desc: 'CANVA PRO Member 1 bulan' }
+  { code: 'P1', name: 'NETFLIX', price: 14500, desc: 'NETFLIX Sharing 1p2u' },
+  { code: 'p2', name: 'CAPCUT', price: 10000, desc: 'CAPCUT Private 1 bulan gar 7h' },
+  { code: 'p3', name: 'CANVA', price: 1500, desc: 'CANVA PRO Member 1 bulan' }
 ];
